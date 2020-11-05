208e9fe6b10805eb35f59ab08c1a58f083cb9cc8
stephen.carnall <stephen.carnall@phe.gov.uk>
2020-11-05 09:25:52 +0000

    initial commit

---
+++ R/test_func.R
@@ -0,0 +1,11 @@
+#'
+#' @title SGSS test function
+#'
+#'
+#' @param data data frame to print - purely to proove the package has been installed correctly
+#' @export
+#'
+
+test_func <- function(data){
+  print(data[,1])
+}
